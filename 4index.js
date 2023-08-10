52bb805aa6d815a0d896d3ca553de623af4f93c6
vishalverma9572 <129514369+vishalverma9572@users.noreply.github.com>
2023-08-10 16:14:41 +0530

    added all file
    all file are uploaded

---
+++ 4index.js
@@ -0,0 +1,9 @@
+btnham=document.getElementById("hamburger");
+btnham.addEventListener("click",function(){
+    document.getElementById("navul").classList.toggle("cmpheigh")
+    // if(document.getElementById("navul").style.height!="0"){
+    //     btnham.innerHTML='<i class="fa-solid fa-2x fa-xmark"></i>'
+        
+    // }
+    
+})
